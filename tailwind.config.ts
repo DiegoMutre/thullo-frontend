3487f63ce1bfb40aa41b5bc4bb6cbe0985b6dd58
DiegoMutre <tysonmutre10@gmail.com>
2023-09-06 18:16:56 -0500

    * Override a color blue property of the tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
+import colors from 'tailwindcss/colors';
 
 const config: Config = {
   content: [
@@ -6,7 +8,19 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  // When overriding a value of the `theme` object, is important to add the `defaultTheme` and the values overridden
+  // Otherwise, the default values won't be included
+  // Although, the `extend` can be used to add another color, but in this case I (Diego) want to override the `600` property of the blue color
+  theme: {
+    ...defaultTheme,
+    colors: {
+      ...colors,
+      blue: {
+        ...colors.blue,
+        '600': '#2F80ED',
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
