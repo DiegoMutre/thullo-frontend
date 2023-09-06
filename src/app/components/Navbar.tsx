75b0e0f731a67379c4a9a68b74a8e8f8da98c123
DiegoMutre <tysonmutre10@gmail.com>
2023-09-06 18:18:08 -0500

    * Create search bar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -11,9 +11,19 @@ export const Navbar = () => {
         className='cursor-pointer'
       />
       <div className='flex gap-12'>
-        <form>
-          <input type='search' placeholder='Keyword...' />
-          <button type='submit'>Search</button>
+        {/* For now using an arbitrary value for the shadow */}
+        <form className='rounded-lg shadow-[0_4px_12px_rgb(0,0,0,0.10)]'>
+          <input
+            className='outline-none py-2 px-4 rounded-lg'
+            type='search'
+            placeholder='Keyword...'
+          />
+          <button
+            className='bg-blue-600 py-2 px-4 text-white rounded-lg font-medium text-sm'
+            type='submit'
+          >
+            Search
+          </button>
         </form>
         <div>
           {/* TODO User avatar here */}
