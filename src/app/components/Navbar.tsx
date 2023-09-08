ce8beba589d8f3b6e01e86448511fad0fc249363
DiegoMutre <tysonmutre10@gmail.com>
2023-09-07 21:17:17 -0500

    * Refactor with new Button component and use shadows from theme

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,10 +1,11 @@
+import { Button } from '@/app/components';
 import { notoSans } from '@/app/utils/fonts';
 import Image from 'next/image';
 import { MdOutlineArrowDropDown } from 'react-icons/md';
 
 export const Navbar = () => {
   return (
-    <nav className='flex justify-between py-6 px-6 bg-white shadow-[0_2px_2px_0_rgba(0,0,0,0.05)]'>
+    <nav className='flex justify-between py-6 px-6 bg-white shadow-sm'>
       <Image
         src={'/Logo.svg'}
         alt='logo'
@@ -13,19 +14,15 @@ export const Navbar = () => {
         className='cursor-pointer'
       />
       <div className='flex gap-12 items-center'>
-        {/* For now using an arbitrary value for the shadow */}
-        <form className='rounded-lg shadow-[0_4px_12px_rgb(0,0,0,0.10)]'>
+        <form className='rounded-lg flex shadow-md'>
           <input
             className='outline-none py-2 px-4 rounded-lg'
             type='search'
             placeholder='Keyword...'
           />
-          <button
-            className='bg-blue-600 py-2 px-4 text-white rounded-lg font-medium text-sm'
-            type='submit'
-          >
+          <Button className='text-sm' variant='primary' type='submit'>
             Search
-          </button>
+          </Button>
         </form>
         <div className='flex items-center gap-2'>
           <Image
