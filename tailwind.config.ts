62837245ab90deca89c1cab0097e5c5e1624adb1
DiegoMutre <tysonmutre10@gmail.com>
2023-09-07 19:46:07 -0500

    * Extend new colors instead of overriding the theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,16 +8,14 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  // When overriding a value of the `theme` object, is important to add the `defaultTheme` and the values overridden
-  // Otherwise, the default values won't be included
-  // Although, the `extend` can be used to add another color, but in this case I (Diego) want to override the `600` property of the blue color
   theme: {
-    ...defaultTheme,
-    colors: {
-      ...colors,
-      blue: {
-        ...colors.blue,
-        '600': '#2F80ED',
+    extend: {
+      colors: {
+        blue1: '#2F80ED',
+        gray1: '#f2f2f2',
+        gray2: '#e0e0e0',
+        gray3: '#828282',
+        gray4: '#bdbdbd',
       },
     },
   },
