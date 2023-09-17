fd7721e4f8f8e2a7b3af1d6ebf7e9a616a44bae6
DiegoMutre <tysonmutre10@gmail.com>
2023-09-17 12:09:12 -0500

    * Setup login form authentication through the UI

--- src/app/login/components/LoginForm.tsx
+++ src/app/login/components/LoginForm.tsx
@@ -3,6 +3,10 @@ import { MdEmail, MdLock } from 'react-icons/md';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useMutation } from '@tanstack/react-query';
+import axios, { isAxiosError } from 'axios';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 import { Button, TextField } from '@/app/components';
 
@@ -28,9 +32,45 @@ export const LoginForm = () => {
     resolver: yupResolver(schema),
   });
 
-  const onSubmit: SubmitHandler<LoginInputs> = data => {
-    // TODO Send request to the api
-    console.log('Sending data...', data);
+  const { mutateAsync } = useMutation({
+    mutationFn: async (data: LoginInputs) => {
+      return await axios.post('http://localhost:4000/api/auth/login', data, {
+        withCredentials: true, // Allow receiving cookies
+      });
+    },
+  });
+
+  const [formErrorMessage, setFormErrorMessage] = useState<string | null>(null);
+
+  const router = useRouter();
+
+  const onSubmit: SubmitHandler<LoginInputs> = async data => {
+    try {
+      // Send request to the login endpoint
+      // TODO Do something with the data returned
+      await mutateAsync(data);
+      // Go to boards page (home page)
+      router.push('/');
+    } catch (error) {
+      // Check if is an axios error and get typescript types about the error
+      if (isAxiosError(error)) {
+        // Catch invalid credentials error
+        if (error.response?.status === 401) {
+          setFormErrorMessage(error.response?.data.message);
+          return;
+        }
+
+        // Catch server errors (500) and network errors (when the API is off)
+        if (
+          (error.response?.status && error.response.status >= 500) ||
+          error.code === 'ERR_NETWORK'
+        ) {
+          // Display message about server error
+          setFormErrorMessage('There was an error, try again later');
+          return;
+        }
+      }
+    }
   };
 
   return (
@@ -58,6 +98,9 @@ export const LoginForm = () => {
       >
         Login
       </Button>
+      {formErrorMessage ? (
+        <span className='text-red-400'>{formErrorMessage}</span>
+      ) : null}
     </form>
   );
 };
