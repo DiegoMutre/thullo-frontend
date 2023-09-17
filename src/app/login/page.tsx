54d78657d53f2511acac50898931b44b004f9cef
DiegoMutre <tysonmutre10@gmail.com>
2023-09-17 10:46:19 -0500

    * Create login page
    As you can see this is alike to the sign up page,
    so this can be improved to avoid code repetition.
    However, it could be a premature optimization because
    it isn't necessary or important to do it now instead when it starts
    to become an obstacle for the maintenance of the code

---
+++ src/app/login/page.tsx
@@ -0,0 +1,37 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { notoSans } from '@/app/utils/fonts';
+
+const LoginPage = () => {
+  return (
+    <main className='flex h-screen items-center'>
+      <div className='mx-auto flex w-1/3 flex-col gap-6 rounded-3xl border border-gray4 bg-white px-12 py-10'>
+        <Image
+          src='/Logo.svg'
+          alt='logo'
+          height={88}
+          width={88}
+          priority
+        />
+        <h2
+          className={`text-xl font-semibold text-[#333] ${notoSans.className}`}
+        >
+          Login
+        </h2>
+        {/* Login form here */}
+        <span className='text-center'>
+          Don&apos;t have an account?{' '}
+          <Link
+            className='text-blue-500 hover:underline'
+            href='/signup'
+          >
+            Signup here
+          </Link>
+        </span>
+      </div>
+    </main>
+  );
+};
+
+export default LoginPage;
