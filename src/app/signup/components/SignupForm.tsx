55ed23df4bc432d57945c81de703c9879b5bda23
DiegoMutre <tysonmutre10@gmail.com>
2023-09-21 10:32:09 -0500

    * Refactor signUp form with new api endpoint
    Also, after authenticating, it logs in the user

--- src/app/signup/components/SignupForm.tsx
+++ src/app/signup/components/SignupForm.tsx
@@ -1,28 +1,15 @@
 'use client';
 import { MdPerson, MdEmail, MdLock } from 'react-icons/md';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { signIn } from 'next-auth/react';
 
 import { Button, TextField } from '@/app/components';
-
-const schema = yup.object({
-  username: yup.string().required('Username is required'),
-  email: yup
-    .string()
-    .required('Email is required')
-    .email('Please enter a valid email address'),
-  password: yup
-    .string()
-    .required('Password is required')
-    .min(8, 'Password must be at least 8 characters long'),
-});
-
-type Inputs = yup.InferType<typeof schema>;
+import { UserInput, userSchema } from '@/app/schemas';
 
 export const SignupForm = () => {
   const {
@@ -31,45 +18,66 @@ export const SignupForm = () => {
     setError,
     formState: { errors },
   } = useForm({
-    resolver: yupResolver(schema),
+    resolver: yupResolver(userSchema),
   });
 
   const mutation = useMutation({
-    mutationFn: async (data: Inputs) => {
+    mutationFn: async (data: UserInput) => {
       // Delete the previous server error
-      setServerErrorMsg(null);
-      return await axios.post('http://localhost:4000/api/auth/signup', data, {
-        withCredentials: true, // Allows to receive cookies, see https://stackoverflow.com/questions/36824106/express-doesnt-set-a-cookie
-      });
+      setFormError(null);
+      return await axios.post('http://localhost:3000/api/signup', data);
     },
   });
 
   const router = useRouter();
-  const [serverErrorMsg, setServerErrorMsg] = useState<string | null>(null);
+  const [formError, setFormError] = useState<string | null>(null);
 
-  const onSubmit: SubmitHandler<Inputs> = async data => {
-    // Send request to the API
+  const onSubmit: SubmitHandler<UserInput> = async data => {
     try {
+      // Send request to the API
       const res = await mutation.mutateAsync(data);
 
-      // If the response is 200, then is `Email already used` response
-      if (res.status === 200) {
-        setError('email', {
-          message: 'Email already used',
-        });
-        return;
-      }
-      // Redirect to the home page /
       if (res.status === 201) {
-        router.push('/');
+        // Automatically sign in when signing up
+        const signInRes = await signIn('credentials', {
+          email: res.data.email,
+          password: data.password, // Use the password from the data coming from the form because the request doesn't return the password
+          redirect: false,
+        });
+
+        // If there's an error when signing in, show this message
+        // This must be handled by the developer (me)
+        if (signInRes?.error) {
+          setFormError('There was an unexpected error, please try again later');
+          return;
+        }
+
+        // Redirect to the app page /app
+        router.push('/app');
       }
     } catch (error) {
       // Check if the error is an axios type error
       if (axios.isAxiosError(error)) {
-        // The previous function transforms the `error` in a `AxiosError` type
-        if (error.response?.status && error.response.status >= 500) {
-          // Show an error message
-          setServerErrorMsg('There was an unexpected error, try again later');
+        // Check if the response exists because it can be undefined
+        if (error.response) {
+          // TODO Use constants here instead of plan strings that can change
+          if (error.response.data.error === 'EmailAlreadyUsed') {
+            setError('email', {
+              message: 'Email already used',
+            });
+            return;
+          }
+
+          // TODO Use constants here instead of plan strings that can change
+          if (error.response.data.error === 'ValidationError') {
+            setFormError('There was an error with the fields, try again later');
+            return;
+          }
+
+          // If there's a server error (5xx) display that message
+          if (error.response.status >= 500) {
+            setFormError('There was an unexpected error, try again later');
+          }
         }
       }
     }
@@ -107,9 +115,7 @@ export const SignupForm = () => {
       >
         Create account
       </Button>
-      {serverErrorMsg ? (
-        <span className='text-red-400'>{serverErrorMsg}</span>
-      ) : null}
+      {formError ? <span className='text-red-400'>{formError}</span> : null}
     </form>
   );
 };
